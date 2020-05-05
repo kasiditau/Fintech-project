c05d48ab552b61efa222838733fa1f3f503b24ae
kasiditau <58271450+kasiditau@users.noreply.github.com>
2020-05-05 19:28:53 +0700

    Add files via upload

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 14 01:52:32 2020
-
 @author: user
 """
 
@@ -10,7 +9,7 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
-
+from sqlalchemy.sql import func
 engine = create_engine('sqlite:///database.db', echo=True)
 Base = declarative_base()
 #######################################
@@ -19,14 +18,36 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    lastname  = Column(String)
     username = Column(String)
     password = Column(String)
     
     #----------------------------------------------------------------------
-    def __init__(self, username, password):
+    def __init__(self,firstname,lastname, username, password):
         """"""
+        self.firstname = firstname
+        self.lastname = lastname
         self.username = username
         self.password = password
+        
+class transaction(Base):
+    """"""
+    __tablename__ = "transactions"
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    payment_type = Column(String)
+    amount = Column(Integer)
+    date = Column(DateTime(timezone=True), server_default=func.now())
+    #----------------------------------------------------------------------
+    def __init__(self,username,payment_type, amount, date):
+        """"""
+        self.username = username
+        self.payment_type = payment_type
+        self.amount = amount
+        self.date = date
 
+                
 # create tables
 Base.metadata.create_all(engine)
